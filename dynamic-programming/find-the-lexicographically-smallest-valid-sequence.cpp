658ed9bfa9b8391e66b7360ecb1d76a2839179f0
Shruti Sharma <shrutisharma260905@gmail.com>
2026-08-08 22:20:35 +0530

    LC-find-the-lexicographically-smallest-valid-sequence [Runtime: N/A; Memory: N/A]

--- dynamic-programming/find-the-lexicographically-smallest-valid-sequence.cpp
+++ dynamic-programming/find-the-lexicographically-smallest-valid-sequence.cpp
@@ -1,49 +1,39 @@
 class Solution {
 public:
     vector<int> validSequence(string word1, string word2) {
-        int n = word1.size(), m = word2.size();
+        int n1 = word1.size(), n2 = word2.size();
 
-        // suf[i] = maximum suffix length of word2 that can be matched
-        // using word1 starting from i.
-        vector<int> suf(n + 1, 0);
+        // suf[i] = smallest index in word2 that still needs to be matched
+        // using word1[i...]
+        vector<int> suf(n1 + 1);
+        suf[n1] = n2;
 
-        int j = m - 1;
-        for (int i = n - 1; i >= 0; i--) {
-            suf[i] = suf[i + 1];
+        int j = n2 - 1;
+        for (int i = n1 - 1; i >= 0; i--) {
             if (j >= 0 && word1[i] == word2[j]) {
                 j--;
-                suf[i]++;
             }
+            suf[i] = j + 1;
         }
 
-        // Even after one modification, impossible.
-        if (suf[0] < m - 1)
-            return {};
-
         vector<int> ans;
-        bool changed = false;
-
+        bool used = false;
         j = 0;
 
-        for (int i = 0; i < n && j < m; i++) {
-
+        for (int i = 0; i < n1; i++) {
             if (word1[i] == word2[j]) {
                 ans.push_back(i);
                 j++;
+                if (j == n2) return ans;
             }
-            else if (!changed) {
-                // Can we spend our one modification here?
-                if (j + suf[i + 1] + 1 >= m) {
-                    changed = true;
-                    ans.push_back(i);
-                    j++;
-                }
+            else if (!used && suf[i + 1] <= j + 1) {
+                used = true;
+                ans.push_back(i);
+                j++;
+                if (j == n2) return ans;
             }
         }
 
-        if (j != m)
-            return {};
-
-        return ans;
+        return {};
     }
 };

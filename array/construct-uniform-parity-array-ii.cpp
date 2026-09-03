52354f62085a53512b12949b3addcb64c97d8447
Shruti Sharma <shrutisharma260905@gmail.com>
2026-09-03 22:40:22 +0530

    LC-construct-uniform-parity-array-ii [Runtime: N/A; Memory: N/A]

---
+++ array/construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        bool allEven = true;
+        for(int x:nums1){
+            if(x%2!=0) allEven = false;
+        }
+        return allEven || (*min_element(nums1.begin(), nums1.end()) % 2 == 1);
+    }
+};

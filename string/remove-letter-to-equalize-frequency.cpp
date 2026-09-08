db36f5d2f23a3afb99c8e474a690f05907f540ad
Shruti Sharma <shrutisharma260905@gmail.com>
2026-09-09 00:21:10 +0530

    LC-remove-letter-to-equalize-frequency [Runtime: N/A; Memory: N/A]

---
+++ string/remove-letter-to-equalize-frequency.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool equalFrequency(string word) {
+        map<char, int>mpp;
+        for(int i=0; i<word.length(); i++){
+            mpp[word[i]]++;
+        }
+        int count=0;
+        for(auto it:mpp){
+            if(it.second>=2) count++;
+        }
+        if(count==1) return true;
+        else return false;
+    }
+};

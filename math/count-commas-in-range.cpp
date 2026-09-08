d96d945f04280d4e59e895280d288fde6e3a4c1d
Shruti Sharma <shrutisharma260905@gmail.com>
2026-09-08 23:21:32 +0530

    LC-count-commas-in-range [Runtime: N/A; Memory: N/A]

--- math/count-commas-in-range.cpp
+++ math/count-commas-in-range.cpp
@@ -4,9 +4,9 @@ public:
         if(n<=999) return 0;
         if(n>=1000 && n<=1999) return n%1000 + 1;
 
-        int s = n-1000;
-        int r = (n%1000)+1 ;
+        //int s = n-1000;
+       // int r = (n%1000)+1 ;
 
-        return s+r;
+        return n - 1000 + 1;
     }
 };
